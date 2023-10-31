4f4210ead7f500e6d35a3e554f62aef8053f2dc6
peterrrr8778 <146065025+peterrrr8778@users.noreply.github.com>
2023-10-31 14:30:41 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int min;
+    float money;
+    scanf("%d",&min);
+    if (min<=800){
+        money=min*0.9;
+    }
+    else if (min>800&&min<1500){
+        money=min*0.9*0.9;
+    }
+    else {
+        money=min*0.9*0.79;
+    }
+    printf("%.1f",money);
+}

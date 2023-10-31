ca90bfde6bed7087a7c12f1de7fafdeaa30a5267
peterrrr8778 <146065025+peterrrr8778@users.noreply.github.com>
2023-10-31 17:59:44 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int h;
+    int min;
+    float angle;
+    scanf("%d,%d",&h,&min);
+    if (min!=0){
+        angle=h*30-(11*min)/2.0;
+    }
+    else{
+        (angle=360-(h*30-(11*min)/2.0));
+    }
+    printf("%.3f",angle);
+}
+
 
